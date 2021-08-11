76d0c11d0b1e9dca6548faa3e3ff8002ed3f1086
RamVegiraju <rav3sa@virginia.edu>
2021-08-11 19:25:35 -0400

    Translate text

---
+++ translate.sh
@@ -0,0 +1,5 @@
+aws translate translate-text \
+            --region us-east-1 \
+            --source-language-code "en" \
+            --target-language-code "es" \
+            --text "hello, world"
