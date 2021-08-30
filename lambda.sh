0fb93188a64c2d7df8b9cd9379bac1532858c47c
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2021-08-30 16:14:56 -0400

    Update lambda.sh

--- lambda.sh
+++ lambda.sh
@@ -1,2 +1,5 @@
 #Listing Lambda functions
 aws lambda list-functions --region us-east-1
+
+#Invoking Lambda functions (output.txt is output file with contents)
+aws lambda invoke --function-name sampleFunction output.txt
