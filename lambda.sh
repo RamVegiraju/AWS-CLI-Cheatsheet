003a2b9f9a884ef17a305b986f7acb0d0592efca
Ram Vegiraju <48113975+RamVegiraju@users.noreply.github.com>
2022-07-13 23:11:29 -0400

    Update lambda.sh

--- lambda.sh
+++ lambda.sh
@@ -3,3 +3,10 @@ aws lambda list-functions --region us-east-1
 
 #Invoking Lambda functions (output.txt is output file with contents)
 aws lambda invoke --function-name sampleFunction output.txt
+
+#Creating a Lambda function with zip-file
+aws lambda create-function --function-name <Insert func name> --role <Insert Lambda Role ARN> 
+--runtime python3.9 --zip-file <Zip file with code> --handler <File with handler func>
+
+#Adding a function URL to existing function
+aws lambda create-function-url-config --function-name <function name> --auth-type NONE
